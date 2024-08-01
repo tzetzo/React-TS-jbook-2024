10fbad685adcb4cfe48c1c637d25627072af180c
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-08-01 19:57:09 +0300

    initial setup of React, TS, esbuild and custom esbuild plugin

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from "react";
+import * as esbuild from 'esbuild-wasm';
+
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const esbuildServiceRef = useRef<esbuild.Service | null>(null);
+
+  useEffect(() => {
+    const startService = async () => {
+      esbuildServiceRef.current = await esbuild.startService({ 
+      worker: true,
+      wasmURL: '/esbuild.wasm' // the file in the public folder we copied from node_modules
+    });
+
+  }
+    startService();
+  }, []);
+
+  
+
+  const onClick = async () => {
+    if(!esbuildServiceRef.current) return;
+
+
+    // const result = await esbuildServiceRef.current.transform(input, { //transform means transpile(from JSX into JS)
+    //   loader: 'jsx',
+    //   target: 'es2015'
+    // });
+    const result = await esbuildServiceRef.current.build({
+      entryPoints: ['index.js'], // we want index.js to be the first file bundled in our app
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"', //replaces process.env.NODE_ENV with the string "production"
+        global: "window" //replaces the var global with the var window
+      }
+    });
+    
+    setCode(result.outputFiles[0].text)
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,69 @@
+import axios from "axios";
+import * as esbuild from "esbuild-wasm";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: "unpkg-path-plugin",
+    setup(build: esbuild.PluginBuild) {
+      // figure out where the entryPoint (index.js as specified in App.tsx) is stored as well as any import
+      build.onResolve({ filter: /.*/ }, async (args) => {
+        if (args.path === "index.js")
+          return { path: args.path, namespace: "a" };
+
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
+            namespace: "a",
+          };
+        }
+
+        return {
+          path: `https://unpkg.com/${args.path}`,
+          namespace: "a",
+        };
+      });
+
+      // load the index.js and any import modules
+      build.onLoad({ filter: /.*/ }, async (args) => {
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: `
+              import React from 'react';
+              import ReactDOM from 'react-dom';
+              console.log(React, ReactDOM);
+            `,
+          };
+        }
+
+        // Check to see if we have already fetched this file and if it is in the cahce
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        // If it is in the cahce return it immediately
+        if (cachedResult) return cachedResult;
+
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        // Store response in the cahce
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+    },
+  };
+};
