9207f925c1b80f6dccfd79038a0bc28e1331c930
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-09-13 22:30:42 +0300

    added monaco-editor instead of textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+
 import { useEffect, useRef, useState } from "react";
-import * as esbuild from 'esbuild-wasm';
+import * as esbuild from "esbuild-wasm";
 
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
 
 function App() {
   const [input, setInput] = useState("");
@@ -11,19 +14,16 @@ function App() {
 
   useEffect(() => {
     const startService = async () => {
-      esbuildServiceRef.current = await esbuild.startService({ 
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm' // the file in the public folder we copied from node_modules
-    });
-
-  }
+      esbuildServiceRef.current = await esbuild.startService({
+        worker: true,
+        wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm", // the file in the public folder we copied from node_modules
+      });
+    };
     startService();
   }, []);
 
-  
-
   const onClick = async () => {
-    if(!esbuildServiceRef.current) return;
+    if (!esbuildServiceRef.current) return;
 
     iframe.current.srcdoc = html;
 
@@ -32,17 +32,17 @@ function App() {
     //   target: 'es2015'
     // });
     const result = await esbuildServiceRef.current.build({
-      entryPoints: ['index.js'], // we want index.js to be the first file bundled in our app
+      entryPoints: ["index.js"], // we want index.js to be the first file bundled in our app
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-        'process.env.NODE_ENV': '"production"', //replaces process.env.NODE_ENV with the string "production"
-        global: "window" //replaces the var global with the var window
-      }
+        "process.env.NODE_ENV": '"production"', //replaces process.env.NODE_ENV with the string "production"
+        global: "window", //replaces the var global with the var window
+      },
     });
-    
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
 
   const html = `
@@ -63,18 +63,27 @@ function App() {
         </script>
       </body>
     </html>
-  `
+  `;
 
   return (
     <div>
-      <textarea
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
+      {/* <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+      ></textarea> */}
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      <iframe
+        title="code preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 }
---
+++ src/components/code-editor.tsx
@@ -0,0 +1,69 @@
+import "./code-editor.css";
+
+import MonacoEditor, { OnMount } from "@monaco-editor/react";
+import * as prettier from "prettier";
+import parserBabel from "prettier/plugins/babel"; // for parsing advanced JS syntax
+import * as prettierPluginEstree from "prettier/plugins/estree";
+import { useRef } from "react";
+
+interface CodeEditorProps {
+  initialValue: string;
+  //   onChange: (value: string) => void; OR
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>(null);
+
+  const handleEditorDidMount: OnMount = (editor) => {
+    editorRef.current = editor;
+    editor.updateOptions({ tabSize: 2 });
+  };
+
+  const onFormatClick = async () => {
+    // get current value from editor
+    const unformatted = editorRef.current.getValue();
+    // format that value
+    const formatted = await prettier.format(unformatted, {
+      parser: "babel",
+      plugins: [parserBabel, prettierPluginEstree],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+    // set the formatted value back in the editor
+    // remove the extra new line char which the editor adds by default
+    editorRef.current.setValue(formatted.replace(/\n$/, ""));
+  };
+
+  return (
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        value={initialValue}
+        onMount={handleEditorDidMount}
+        onChange={(value) => onChange(value || "")}
+        theme="vs-dark"
+        language="javascript"
+        height="500px"
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
