b68682b8be532b715ffcc061540961d4e3a641b5
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-09-17 15:42:31 +0300

    extract code editor and preview window into a separate component. substitute anonymous function with named one for debugging purposes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
-import { useState } from "react";
-
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
-import bundle from "./bundler";
+import CodeCell from "./components/code-cell";
 
 function App() {
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 }
