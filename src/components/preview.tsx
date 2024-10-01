ba34f16350f7cba62658bc86ede1688df91cc1f1
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-10-01 14:41:19 +0300

    debouncing logic for code bundling

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -30,8 +30,8 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
-    // iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+    iframe.current.srcdoc = html;
+    setTimeout(() => iframe.current.contentWindow.postMessage(code, "*"), 50);
   }, [code]);
 
   return (
